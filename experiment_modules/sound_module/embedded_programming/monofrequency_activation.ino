f0f084b0cd1a0448fb4b24914d1894d973f0ae98
Andres Rico <32314497+AndresRicoM@users.noreply.github.com>
2021-05-27 11:27:18 -0400

    Update monofrequency_activation.ino

--- experiment_modules/sound_module/embedded_programming/monofrequency_activation.ino
+++ experiment_modules/sound_module/embedded_programming/monofrequency_activation.ino
@@ -1,10 +1,14 @@
 /*
+
 ███╗   ███╗ █████╗ ██╗ █████╗
 ████╗ ████║██╔══██╗██║██╔══██╗
 ██╔████╔██║███████║██║███████║
 ██║╚██╔╝██║██╔══██║██║██╔══██║
 ██║ ╚═╝ ██║██║  ██║██║██║  ██║
 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝
+
+Code for running a mono frequency sound for running sound experiments on MAIA rector's sound experiment module. 
+
  */
 
 int soundPin = 3; //Output Pin on ATTINY 412. 
