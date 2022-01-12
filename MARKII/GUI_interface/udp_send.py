6d4210b81ce7e35b80a81fae53cfea5a58b8af8f
aricom <aricom@mit.edu>
2022-01-12 18:02:28 -0500

    added udp communication for temperature tests.

---
+++ MARKII/GUI_interface/udp_send.py
@@ -0,0 +1,13 @@
+import socket
+
+while True:
+    UDP_IP = "10.31.110.80"
+    UDP_PORT = 1206
+    MESSAGE = input("Enter your value: ")
+
+    print("UDP target IP:", UDP_IP)
+    print("UDP target port:", UDP_PORT)
+    print("message:", MESSAGE)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
