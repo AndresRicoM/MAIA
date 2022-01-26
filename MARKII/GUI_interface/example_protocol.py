da32217932a79c704e1dabb14b282160ac6e3499
aricom <aricom@mit.edu>
2022-01-26 14:07:32 -0500

    added light + temp control protocol.

--- MARKII/GUI_interface/example_protocol.py
+++ MARKII/GUI_interface/example_protocol.py
@@ -1,9 +1,10 @@
 import socket
+import time
 
-while True:
+def send_udp(message):
     UDP_IP = "10.31.67.9"
     UDP_PORT = 1206
-    MESSAGE = input("Enter your value: ")
+    MESSAGE = message
 
     print("UDP target IP:", UDP_IP)
     print("UDP target port:", UDP_PORT)
@@ -11,3 +12,12 @@ while True:
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+
+
+#Set Temperature to 37  Celcius
+print('Hello, Welcome to MAIA!')
+print('Running Example Growth and Light Stimulation Protocol')
+print('Setting Temperature')
+send_udp('T50')
+print('Stimulating with Red Light')
+send_udp('L2')
